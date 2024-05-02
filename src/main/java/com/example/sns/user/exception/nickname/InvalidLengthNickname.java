be93103b2a4aab10c7426aa21ee56862bee50dd1
kimjinsu <tjtfy7028@gmail.com>
2024-05-02 15:28:28 +0900

    style: 닉네임 길이 예외 클래스명 변경

--- src/main/java/com/example/sns/user/exception/nickname/InvalidLengthNickname.java
+++ src/main/java/com/example/sns/user/exception/nickname/InvalidLengthNickname.java
@@ -1,4 +1,4 @@
-package com.example.sns.user.exception;
+package com.example.sns.user.exception.nickname;
 
 import com.example.sns.common.exception.RootException;
 import com.example.sns.user.domain.entity.Nickname;
@@ -6,11 +6,11 @@ import com.example.sns.user.domain.entity.Nickname;
 /**
  * status : 400
  */
-public class InvalidNicknameLength extends RootException {
+public class InvalidLengthNickname extends RootException {
 
     private static final String MESSAGE = Nickname.LENGTH_MIN+"글자 이상" +","+Nickname.LENGTH_MAX+" 글자 이하로 입력해주세요.";
 
-    public InvalidNicknameLength() {
+    public InvalidLengthNickname() {
         super(MESSAGE);
     }
 
